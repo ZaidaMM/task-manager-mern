5a9bc377f7879fbfc0690b5fc0d1ec08dd575fa7
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-06 17:08:06 +0100

    Frontend. Routes

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navigation from './components/Navigation';
+import Intro from './components/Intro';
+import TasksList from './components/TasksList';
+import AddTask from './components/AddTask';
+import AddUser from './components/AddUser';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+
+      <Routes>
+        <Route exact path='/' element={<Intro />} />
+        <Route path='/list' element={<TasksList />} />
+        <Route path='/edit/:id' element={<AddTask />} />
+        <Route path='/add' element={<AddTask />} />
+        <Route path='/user' element={<AddUser />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TasksList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class TasksList extends Component {
+  render() {
+    return <div>TasksList</div>;
+  }
+}
